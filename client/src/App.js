492202d9f1135edded5ba0952a63db908262f125
nataliamachella <nataliamachella@gmail.com>
2022-12-16 09:19:50 -0300

    card de recetas modificadas

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,16 @@ import React, { useEffect, useState } from "react";
 import Recipe from "./components/Recipe";
 import axios from "axios";
 import { Grid } from "@mui/material";
-import SearchAppBar from "./components/Navbar";
+import styled from "styled-components";
+import IconButton from "@mui/material/IconButton";
+import Paper from "@mui/material/Paper";
+import InputBase from "@mui/material/InputBase";
+import SearchIcon from "@mui/icons-material/Search";
 
 const App = () => {
   const [recipes, setRecipes] = useState([]);
 
-  useEffect(() => {
-    getRecipes();
-  }, []);
+  const [query, setQuery] = useState("");
 
   const getRecipes = async () => {
     const response = await axios.get(`http://localhost:5000/recipes`);
@@ -17,9 +19,70 @@ const App = () => {
     setRecipes(response.data);
   };
 
+  useEffect(() => {
+    getRecipes();
+  }, []);
+
+  const searchRecipes = async () => {
+    const response = await axios.get(`http://localhost:5000/recipes/${query}`);
+    console.log(response.data);
+    setRecipes(response.data);
+  };
+
+  const changeHandler = (e) => {
+    setQuery(e.target.value);
+  };
+
+  const AppName = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+  `;
+  const Header = styled.div`
+    background-color: purple;
+    color: white;
+    display: flex;
+    justify-content: space-between;
+    flex-direction: row;
+    align-items: center;
+    padding: 20px;
+    font-size: 25px;
+    font-weight: bold;
+    box-shadow: 0 3px 6px 0 #555;
+  `;
+
   return (
     <div>
-      <SearchAppBar />
+      <Header>
+        <AppName>Recipes Finder</AppName>
+        <Paper
+          component="form"
+          sx={{
+            p: "2px 4px",
+            display: "flex",
+            alignItems: "center",
+            width: 400,
+          }}
+          onSubmit={searchRecipes}
+        >
+          <InputBase
+            sx={{ ml: 1, flex: 1 }}
+            placeholder="Search"
+            inputProps={{ "aria-label": "search" }}
+            onChange={changeHandler}
+            type="text"
+            value={query}
+          />
+          <IconButton type="submit" sx={{ p: "10px" }} aria-label="search">
+            <SearchIcon />
+          </IconButton>
+        </Paper>
+      </Header>
+      {/* 
+      <form onSubmit={searchRecipes}>
+        <input type="text" value={query} onChange={changeHandler} />
+        <button type="submit">Search</button>
+      </form> */}
 
       <Grid
         container
@@ -41,6 +104,7 @@ const App = () => {
               title={recipe.recipe.label}
               image={recipe.recipe.image}
               ingredients={recipe.recipe.ingredients}
+              url={recipe.recipe.url}
             />
           </Grid>
         ))}
--- client/src/components/Recipe.js
+++ client/src/components/Recipe.js
@@ -6,14 +6,17 @@ import CardMedia from "@mui/material/CardMedia";
 import CardContent from "@mui/material/CardContent";
 import CardActions from "@mui/material/CardActions";
 import Collapse from "@mui/material/Collapse";
-
+import Modal from "@mui/material/Modal";
 import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
-
-import FavoriteIcon from "@mui/icons-material/Favorite";
+import Box from "@mui/material/Box";
+import FavoriteBorder from "@mui/icons-material/FavoriteBorder";
 import ShareIcon from "@mui/icons-material/Share";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
+
+import Checkbox from "@mui/material/Checkbox";
+import Favorite from "@mui/icons-material/Favorite";
 
 const ExpandMore = styled((props) => {
   const { expand, ...other } = props;
@@ -25,14 +28,28 @@ const ExpandMore = styled((props) => {
     duration: theme.transitions.duration.shortest,
   }),
 }));
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
 
-const Recipe = ({ title, image, ingredients }) => {
+  boxShadow: 24,
+  p: 4,
+};
+
+const Recipe = ({ title, image, ingredients, url }) => {
   const [expanded, setExpanded] = React.useState(false);
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
   const handleExpandClick = () => {
     setExpanded(!expanded);
   };
-
+  const label = { inputProps: { "aria-label": "Checkbox demo" } };
   return (
     <Card sx={{ maxWidth: 345 }} elevation={3}>
       <CardHeader
@@ -45,12 +62,29 @@ const Recipe = ({ title, image, ingredients }) => {
       />
       <CardMedia component="img" height="194" image={image} alt={title} />
       <CardContent>
-        <IconButton aria-label="add to favorites">
-          <FavoriteIcon />
-        </IconButton>
-        <IconButton aria-label="share">
+        <Checkbox
+          {...label}
+          icon={<FavoriteBorder />}
+          checkedIcon={<Favorite />}
+        />
+        <IconButton aria-label="share" onClick={handleOpen}>
           <ShareIcon />
         </IconButton>
+        <Modal
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <Box sx={style}>
+            <Typography id="modal-modal-title" variant="h6" component="h2">
+              Link
+            </Typography>
+            <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+              {url}
+            </Typography>
+          </Box>
+        </Modal>
       </CardContent>
       <CardActions disableSpacing>
         <h4>Ingredients</h4>
